44ed743ec75f8574c9b52abae5cdca63e6e94659
Dale Bustad <dale@divmain.com>
2021-09-25 02:24:30 -0700

    omit redundant allocator selection

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,6 @@ use napi::{JsObject, Result};
 
 mod transpile_2015;
 
-#[cfg(all(
-  any(windows, unix),
-  target_arch = "x86_64",
-  not(target_env = "musl"),
-  not(debug_assertions)
-))]
-#[global_allocator]
-static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
 #[module_exports]
 fn init(mut exports: JsObject) -> Result<()> {
   exports.create_named_method("transpile", transpile_2015::transpile_async)?;
